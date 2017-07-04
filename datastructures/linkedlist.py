1ccb7699f3efcf6042139cf09d2c663b11010f0d
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2017-07-04 15:56:24 +0530

    class LinkedList added

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -2,3 +2,11 @@ class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self._next = next
+
+
+class LinkedList:
+    def __init__(self, data=None):
+        if data is None:
+            self.head = None
+        else:
+            self.head = Node(data)
